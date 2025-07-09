f90ddd7e5fe9f36273b1ec021d499c21f425c78f
Himani Shah <himani.shah201205@gmail.com>
2025-07-09 05:31:46 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <Balance balance={balance} />
+      <Balance balance={balance} className="align-centre" />
       <h2>Transactions</h2> 
       <TransactionList transactions={transactions} />
       <AddTransaction onAdd={addTransaction} />
